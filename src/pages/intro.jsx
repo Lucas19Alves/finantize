bce570be2fe51f91e26080c4da9ffc8e2b19fa9e
Lucas HotelPC <lucaas19alves@gmail.com>
2024-03-15 20:07:19 -0300

    Start

---
+++ src/pages/intro.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import '../index.css'
+
+
+function Intro () {
+
+    return (
+        <>
+        
+        </>
+    )
+
+}
+
+export default Intro
